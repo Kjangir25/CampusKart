1b37d2d10604f7a7562259d668a7f6ddfe9df621
Kjangir25 <jangirkumkum884@gmail.com>
2026-09-20 22:01:02 +0530

    remove backend

--- backend/models/Product.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const productSchema = new mongoose.Schema({
-    title: String,
-    price: Number,
-    description: String,
-    image: String,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-}, { timestamps: true });
-export default mongoose.model("Product", productSchema);
--- backend/routes/products.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import Product from "../models/Product.js";
-const router = express.Router();
-
-router.get("/", async (req, res) => { const p = await Product.find(); res.json(p); });
-router.post("/", async (req, res) => { const p = await Product.create(req.body); res.json(p); });
-
-export default router;
