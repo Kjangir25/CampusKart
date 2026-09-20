0eac6ff95165547227cccab8b93755b80b09face
Kjangir25 <jangirkumkum884@gmail.com>
2026-09-20 22:10:16 +0530

    case error

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./Profile.css";
+import "./profile.css";
 
 const Profile = ({ go, user, setUser }) => {
     const saved = JSON.parse(localStorage.getItem("userProfile"));
